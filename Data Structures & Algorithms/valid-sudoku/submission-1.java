87c932bbe9c557d042268a817dd1315d518d5c31
Abhishek Upadhyaya <72117184+abhishekupadhyaya477@users.noreply.github.com>
2026-08-01 17:58:40 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] row = new HashSet[9];
+        HashSet<Character>[] col = new HashSet[9];
+        HashSet<Character>[][] squares = new HashSet[3][3];
+
+
+        for(int i=0;i<9;i++){
+            row[i] = new HashSet<>();
+            col[i] = new HashSet<>();
+        }
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                squares[i][j] = new HashSet<>();
+            }
+        }
+
+        for(int r=0; r<9; r++){
+            for(int c=0; c<9; c++){
+                
+                char ch = board[r][c];
+
+                if(ch=='.') continue;
+
+                int rc=r/3;
+                int cc=c/3;
+
+                if(row[r].contains(ch)|| col[c].contains(ch) || squares[rc][cc].contains(ch)) return false;
+
+                row[r].add(ch);
+                col[c].add(ch);
+                squares[rc][cc].add(ch);
+            }
+        }
+        return true;
+    }
+}
