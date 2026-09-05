c3fef0c02a9f134f0d6c6de6d4c0db036117f523
Abhishek Upadhyaya <72117184+abhishekupadhyaya477@users.noreply.github.com>
2026-09-06 00:35:11 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int res[] = new int[len];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0; i<len; i++){       
+            while(!st.isEmpty() && temperatures[st.peek()] < temperatures[i]){
+                int currIndex = st.pop();
+                int diff = i - currIndex;
+                res[currIndex] = diff;
+            }
+            st.push(i);
+        }
+
+        return res;
+    }
+}
